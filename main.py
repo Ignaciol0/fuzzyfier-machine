a88fe34baad734ea5fca33013680ed26b9d0ba3c
Javier Martin <jmartinpizarro04@gmail.com>
2024-05-03 09:55:29 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from MFIS_Classes import *
+from MFIS_Read_Functions import *
+import matplotlib.pyplot as plt
+
+fuzzySet = readFuzzySetsFile('InputVarSets.txt')
+rules = readRulesFile()
+applications = readApplicationsFile()
+
+
+
