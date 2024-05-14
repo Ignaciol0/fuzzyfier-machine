cc4828910906138f8ec9abaf5d400e54f716de1c
100495680 <100495680@alumnos.uc3m.es>
2024-05-14 18:14:31 +0200

    Commit Final

--- main.py
+++ main.py
@@ -46,15 +46,22 @@ def applyRules(rules, fuzzified):
             new_y[index] = min(value,risk_fuzzy.y[index])
         risk_fuzzy.y = new_y
     
-    fuzzy_high = risk_fuzzy['Risk=HighR']
-    fuzzy_low = risk_fuzzy['Risk=LowR']
-    fuzzy_med = risk_fuzzy['Risk=MediumR']
+    fuzzy_high = risk_fuzzys['Risk=HighR']
+    fuzzy_low = risk_fuzzys['Risk=LowR']
+    fuzzy_med = risk_fuzzys['Risk=MediumR']
     new_y = list(np.zeros(100))
     for index in range(len(fuzzy_high.y)):
-        new_y[index] = max(fuzzy_high[index],fuzzy_low[index],fuzzy_med[index])
+        new_y[index] = max(fuzzy_high.y[index],fuzzy_low.y[index],fuzzy_med.y[index])
     fuzzy = fuzzy_low
     fuzzy.y = new_y
     return fuzzy
+
+def defuzzify(results, fuzzySets):
+    """Defuzzify the results using the centroid method"""
+    x = results.x
+    trapmf = np.array(results.y)
+    defuzzified = fuzz.defuzz(x, trapmf, 'centroid')
+    return defuzzified
 # Read the fuzzy sets and the rules from the files
 fuzzySets = readFuzzySetsFile('InputVarSets.txt')
 rules = readRulesFile()
@@ -69,7 +76,10 @@ for app in applications:
     # Apply the rules
     results = applyRules(rules, fuzzified)
 
+    # Defuzzify the results
+    defuzzified = defuzzify(results, fuzzySets)
+
     # Print the results
-    text += f"Applicant {applications.index(app)+1}: {results}\n"
+    text += f"Applicant {applications.index(app)+1}: {round(defuzzified,2)} risky score result\n"
 with open("Results.txt",'w') as f:
     f.write(text)
